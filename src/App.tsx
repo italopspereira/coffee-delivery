aa41404379ea34b8b4c030e9aeae1864e0761c6f
Ítalo Pereira <italo.pspereira@gmail.com>
2023-01-12 21:42:13 -0300

    import presentation container

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { Header } from './components/Header/header'
+import { Presentation } from './components/Presentation/presentation'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <Header></Header>
+      <Presentation></Presentation>
     </ThemeProvider>
   )
 }
