8e2b6375ae36101c49794415938d1eb4e0ddf337
Ítalo Pereira <italo.pspereira@gmail.com>
2023-01-24 19:01:50 -0300

    created success route

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,7 +6,7 @@ import {
 import { Header } from './components/Header/header'
 import { Home } from './pages/Home/index'
 import { Checkout } from './pages/Checkout'
-
+import { SuccessPage } from './pages/Success'
 export const Routes = () => {
   return (
     <Router>
@@ -14,6 +14,7 @@ export const Routes = () => {
       <Switch>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<SuccessPage />} />
       </Switch>
     </Router>
   )
